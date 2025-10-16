e5f2dfe28ff25264e221dbc57ce7fbc846c1336d
unknown <shashankbharadwaj2703@gmail.com>
2025-10-16 23:36:19 +0530

    config change

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,15 +7,6 @@ export default defineConfig({
     host: '0.0.0.0', 
     port: 5173,
   },
-    build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name]-[hash].js'.toLowerCase(),
-        chunkFileNames: 'assets/[name]-[hash].js'.toLowerCase(),
-        assetFileNames: 'assets/[name]-[hash][extname]'.toLowerCase(),
-      },
-    },
-  },
   plugins: [react()],
-  base: './',
+  base: '/',
 })
